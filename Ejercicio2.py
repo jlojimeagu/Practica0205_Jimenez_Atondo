a7711dc2cbde688c9c0303fbce39f34c63f79088
juan miguel <jlojimeagu1@educacion.navarra.es>
2022-11-15 12:59:27 +0100

    no hecho

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,10 +1,11 @@
-usuario = {}
+
 while True:
-    user = input("indica nombre,edad,direccion y telefono en ese orden usando el formato: usuario[título de lo indicado = "
-               "lo indicado\n")
-    if user in "terminar":
-       print(usuario['nombre'], "tiene", usuario['edad'],  "años", "vive en", usuario['nombre'], "y su número de teléfono es",
-                    usuario['telefono'])
-       break
+    user = input(
+        "indica nombre,edad,direccion y telefono en ese orden usando el formato: usuario[título de lo indicado] = "
+        "lo indicado\n")usuario = {}
+    if 'terminar' in user:
+        print(usuario['nombre'], "tiene", usuario['edad'], "años", "vive en", usuario['direccion'],
+              "y su número de teléfono es", usuario['telefono'])
+        break
     else:
         print("continúa escribiendo")
