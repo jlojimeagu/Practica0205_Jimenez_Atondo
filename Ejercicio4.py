d65ef5fafe66616f0055ee6c43ba0d00eec184bb
aatondouna <113340871+aatondouna@users.noreply.github.com>
2022-11-15 13:54:07 +0100

    Ejercicio 4 no completado

---
+++ Ejercicio4.py
@@ -0,0 +1,11 @@
+nombre = input("introduce el nombre:\n")
+edad = input("introduce la edad:\n")
+direccion = input("introduce la direccion:\n")
+telefono = input("introduce el numero de telefono:\n")
+correo_electronico = input("introduce el correo electronico:\n")
+
+
+diccionario1 = {
+
+
+
