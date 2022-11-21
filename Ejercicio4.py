f5d317cef1d2058f11faec18e6596965ed98d585
aatondouna <113340871+aatondouna@users.noreply.github.com>
2022-11-21 10:15:08 +0100

    Ejercicio 4 completado

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,11 +1,17 @@
-nombre = input("introduce el nombre:\n")
-edad = input("introduce la edad:\n")
-direccion = input("introduce la direccion:\n")
-telefono = input("introduce el numero de telefono:\n")
-correo_electronico = input("introduce el correo electronico:\n")
+diccionario1 = {}
+diccionario1['nombre'] = input("introduce el nombre:\n")
+print(diccionario1)
+diccionario1['edad'] = input("introduce la edad:\n")
+print(diccionario1)
+diccionario1['direccion'] = input("introduce la direccion:\n")
+print(diccionario1)
+diccionario1['telefono'] = input("introduce el numero de telefono:\n")
+print(diccionario1)
+diccionario1['correo_electronico'] = input("introduce el correo electronico:\n")
+print(diccionario1)
 
 
-diccionario1 = {
+
 
 
 
